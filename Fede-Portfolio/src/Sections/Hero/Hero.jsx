7ccfbdd6eb9d854e4ced6402fa3eab397562a654
Federico Rosales <fedocho.96@gmail.com>
2024-08-14 01:55:17 -0300

    Update Hero.jsx
    Añadido titulo, e imagenes

--- Fede-Portfolio/src/Sections/Hero/Hero.jsx
+++ Fede-Portfolio/src/Sections/Hero/Hero.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import styles from './HeroStyles.module.css'
+import heroImg from '../../assets/hero-img.png'
+import themeIcon from '../../assets/sun.svg'
+import twitterIcon from '../../assets/twitter-light.svg'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id='hero'>
+      <div className={styles.colorModeContainer}>
+        <img className={styles.hero}
+         src={heroImg} 
+         alt="profile picture"
+         />
+        <img className={styles.colorMode}
+         src={themeIcon}
+          alt="Color mode icon"
+        />
+        <div className={styles.info}>
+          <h1>Federico <br/> Rosales</h1>
+          <h2>Fullstack Developer</h2>
+          <span>
+            <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="" />
+            </a>
+          </span>
+        </div>
+      </div>
+    </section>
   )
 }
 
