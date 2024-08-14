53b861c553e537f04c9cd5e9eb4e3ab6bdd25b03
Federico Rosales <fedocho.96@gmail.com>
2024-08-14 02:41:56 -0300

    Iconos y CV
    Agregados Iconos y boton de CV (revisar archivo de cv no compatible)

--- Fede-Portfolio/src/Sections/Hero/Hero.jsx
+++ Fede-Portfolio/src/Sections/Hero/Hero.jsx
@@ -2,6 +2,10 @@ import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/hero-img.png'
 import themeIcon from '../../assets/sun.svg'
 import twitterIcon from '../../assets/twitter-light.svg'
+import githubIcon from '../../assets/github-light.svg'
+import linkedinIcon from '../../assets/linkedin-light.svg'
+import CV from '../../assets/cv.pdf'
+
 
 const Hero = () => {
   return (
@@ -22,7 +26,19 @@ const Hero = () => {
             <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="" />
             </a>
+            <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="" />
+            </a>
+            <a href="https://Linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="" />
+            </a>
           </span>
+          <p>With a passion for music, coffe and developing web apps.
+             Get in touch
+          </p>
+          <a href={CV} download>
+            <button className='hover'> Resume</button>
+          </a>
         </div>
       </div>
     </section>
