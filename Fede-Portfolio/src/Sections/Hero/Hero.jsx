79d3fb8ea4c5f8081cb87e0a4b3a448afe469936
Federico Rosales <fedocho.96@gmail.com>
2024-09-16 17:43:42 -0300

    setting App css
    setting App css

--- Fede-Portfolio/src/Sections/Hero/Hero.jsx
+++ Fede-Portfolio/src/Sections/Hero/Hero.jsx
@@ -1,9 +1,14 @@
 import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/hero-img.png'
 import themeIcon from '../../assets/sun.svg'
-import twitterIcon from '../../assets/twitter-light.svg'
-import githubIcon from '../../assets/github-light.svg'
-import linkedinIcon from '../../assets/linkedin-light.svg'
+import twitterLight from '../../assets/twitter-light.svg'
+import githubLight from '../../assets/github-light.svg'
+import linkedinLight from '../../assets/linkedin-light.svg'
+import twitterDark from '../../assets/twitter-dark.svg'
+import githubDark from '../../assets/github-dark.svg'
+import linkedinDark from '../../assets/linkedin-dark.svg'
+import sun from '../../assets/sun.svg'
+import moon from '../../assets/moon.svg'
 import CV from '../../assets/cv.pdf'
 import { useTheme } from '../../common/ThemeContext'
 
@@ -11,8 +16,11 @@ import { useTheme } from '../../common/ThemeContext'
 const Hero = () => {
   const {theme, toggleTheme} = useTheme();
 
-
-
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  
   return (
     <section id='hero'>
       <div className={styles.colorModeContainer}>
