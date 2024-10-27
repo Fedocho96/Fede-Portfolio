7ccbe90c14f866d336c4f1a53d81db285e4388fa
Federico Rosales <fedocho.96@gmail.com>
2024-10-27 20:08:50 -0300

    cards done
    cards done

--- Fede-Portfolio/src/Sections/Hero/Projects/Projects.jsx
+++ Fede-Portfolio/src/Sections/Hero/Projects/Projects.jsx
@@ -9,9 +9,26 @@ const Projects = () => {
         <h1 className='sectionTitle'>projects</h1>
         <div className={styles.projectsContainer}>
 
-            <ProjectCard src={fedelogo} link={'https://github.com/Fedocho96/Fede-Portfolio'}
-            h3='Portfolio'
-            p='Portfolio app'
+            <ProjectCard
+             src={fedelogo} 
+             link={'https://github.com/Fedocho96/Fede-Portfolio'}
+             h3='Portfolio'
+             p='Portfolio mobile app'
+            />
+
+            <ProjectCard 
+             src={fedelogo} 
+             link={'https://github.com/Fedocho96/Fede-Portfolio'}
+             h3='Portfolio'
+             p='Portfolio app'
+            />
+
+
+            <ProjectCard
+             src={fedelogo}
+             link={'https://github.com/Fedocho96/Fede-Portfolio'}
+             h3='Portfolio'
+             p='Portfolio app'
             />
 
             
